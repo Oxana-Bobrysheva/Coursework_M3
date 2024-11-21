c54a6b0d03d72f8e7f5b9b56bbdfe08e7c043fba
Oxana Bobrysheva <bobrysheva_oxana@mail.ru>
2024-11-21 21:16:19 +0300

    Docstrings were written and lintens checked

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 import json
 import requests
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
 
 def greetings(input_date: str) -> str:
     """Function <greetings> takes a string with date and time in form YYYY-MM-DD HH:MM:SS
@@ -26,7 +28,7 @@ def time_range(input_date: str) -> tuple[str, str]:
     and the <start_time> sets the day as the first day of the indicated month."""
     date_obj = datetime.datetime.strptime(input_date, "%Y-%m-%d %H:%M:%S")
     end_time = date_obj.strftime("%d.%m.%Y")
-    new_date_obj = date_obj.replace (day=1)
+    new_date_obj = date_obj.replace(day=1)
     start_time = new_date_obj.strftime("%d.%m.%Y")
 
     return start_time, end_time
@@ -55,14 +57,19 @@ def filter_by_time_range(operations: list, input_date: str) -> list:
     start_time_obj = datetime.datetime.strptime(start_time, "%d.%m.%Y")
     end_time_obj = datetime.datetime.strptime(end_time, "%d.%m.%Y")
     for operation in operations:
-        if (isinstance(operation["Дата платежа"], str) and start_time_obj <=
-            datetime.datetime.strptime(operation["Дата платежа"], "%d.%m.%Y") <= end_time_obj):
+        if (
+            isinstance(operation["Дата платежа"], str)
+            and start_time_obj
+            <= datetime.datetime.strptime(operation["Дата платежа"], "%d.%m.%Y")
+            <= end_time_obj
+        ):
             filtered_operations.append(operation)
 
     return filtered_operations
 
 
 def get_unique_cards(filtered_transactions):
+    """Function that gets a set of card numbers from filtered list of operations"""
     cards_info: list = []
     if filtered_transactions:
         for transaction in filtered_transactions:
@@ -74,6 +81,8 @@ def get_unique_cards(filtered_transactions):
 
 
 def get_list_of_cards_info(cards_info: list, filtered_transactions: list) -> list[dict]:
+    """Function that gets requested information about cards"""
+
     list_of_cards_info = []
 
     for card in cards_info:
@@ -83,33 +92,43 @@ def get_list_of_cards_info(cards_info: list, filtered_transactions: list) -> lis
 
         for transaction in filtered_transactions:
             if transaction["Номер карты"] == card:
-                total_spent += transaction['Сумма платежа']
+                total_spent += transaction["Сумма платежа"]
                 # Получаем значение кэшбэка, если оно nan, добавляем 0
                 cashback_value = transaction.get("Кэшбэк", 0)
-                if isinstance(cashback_value, (int, float)) and not np.isnan(cashback_value):
+                if isinstance(cashback_value, (int, float)) and not np.isnan(
+                    cashback_value
+                ):
                     cashback += cashback_value
         total_spent = round(total_spent, 2)
-        list_of_cards_info.append({
-            "last_digits": last_digits,
-            "total_spent": total_spent,
-            "cashback": cashback
-        })
+        list_of_cards_info.append(
+            {
+                "last_digits": last_digits,
+                "total_spent": total_spent,
+                "cashback": cashback,
+            }
+        )
 
     return list_of_cards_info
 
 
 def get_top_transactions(filtered_transactions):
+    """Function that gets top-5 operations from the list of operations"""
+
     top_transactions = []
-    sorted_filtered_transactions = sorted(filtered_transactions, key=lambda x: x["Сумма платежа"])
+    sorted_filtered_transactions = sorted(
+        filtered_transactions, key=lambda x: x["Сумма платежа"]
+    )
     i = 0
     for i, transaction in enumerate(sorted_filtered_transactions):
         if i < 5:
-            top_transactions.append({
-                "date": transaction.get("Дата платежа"),
-                "amount": transaction.get("Сумма платежа"),
-                "category": transaction.get("Категория"),
-                "description": transaction.get("Описание")
-            })
+            top_transactions.append(
+                {
+                    "date": transaction.get("Дата платежа"),
+                    "amount": transaction.get("Сумма платежа"),
+                    "category": transaction.get("Категория"),
+                    "description": transaction.get("Описание"),
+                }
+            )
             i += 1
         else:
             break
@@ -117,43 +136,45 @@ def get_top_transactions(filtered_transactions):
 
 
 def load_user_settings(file_path):
-    with open(file_path, 'r') as file:
+    """Function that reads information from json file"""
+
+    with open(file_path, "r") as file:
         return json.load(file)
 
+
 def get_currency_rates(currencies):
-    # Пример использования API для получения курсов валют
+    """Function that makes API request and returns currency information"""
+
     api_key = os.getenv("API_Key_currency")
-    base_currency = 'RUB'  # Базовая валюта
-    url = f'https://api.exchangerate-api.com/v4/latest/{base_currency}'
+    base_currency = "RUB"  # Базовая валюта
+    url = f"https://api.exchangerate-api.com/v4/latest/{base_currency}"
 
     response = requests.get(url)
     data = response.json()
 
-    rates = {currency: data['rates'].get(currency) for currency in currencies}
+    rates = {currency: data["rates"].get(currency) for currency in currencies}
     return rates
 
+
 def get_stock_prices(stocks):
-    # Пример использования Alpha Vantage API для получения цен акций
-    api_key = os.getenv('API_Key_stokes')
+    # Alpha Vantage API for stocks
+    api_key = os.getenv("API_Key_stokes")
     stock_prices = {}
 
     for stock in stocks:
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock}&interval=1min&apikey={api_key}'
+        url = (
+            f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={stock}"
+            f"&interval=1min&apikey={api_key}"
+        )
         response = requests.get(url)
         data = response.json()
 
-        # Получаем последнюю цену
+        # We get mean price
         try:
-            last_refreshed = data['Meta Data']['3. Last Refreshed']
-            last_price = data['Time Series (1min)'][last_refreshed]['1. open']
+            last_refreshed = data["Meta Data"]["3. Last Refreshed"]
+            last_price = data["Time Series (1min)"][last_refreshed]["1. open"]
             stock_prices[stock] = float(last_price)
         except KeyError:
             stock_prices[stock] = None  # Если данные недоступны
 
     return stock_prices
-
-
-# Пример использования
-if __name__ == "__main__":
-    user_data = get_user_data('user_settings.json')
-    print(user_data)
--- src/views.py
+++ src/views.py
@@ -1,39 +1,60 @@
-from dotenv import load_dotenv
+import json
 
-from src.utils import get_unique_cards, get_list_of_cards_info, greetings, filter_by_time_range, read_xlsx, \
-    get_top_transactions, load_user_settings, get_currency_rates, get_stock_prices
+from src.utils import (
+    get_unique_cards,
+    get_list_of_cards_info,
+    greetings,
+    filter_by_time_range,
+    read_xlsx,
+    get_top_transactions,
+    load_user_settings,
+    get_currency_rates,
+    get_stock_prices,
+)
 
 
 def get_started_main():
+    """Function that returns JSON answer for the web page MAIN"""
+    # Function starts with request of the date
     input_date = input("Введите дату в формате YYYY-MM-DD HH:MM:SS - ")
     json_answer = {"greeting": greetings(input_date)}
 
+    # Getting the filtered list of operations from the file according to the time limits
     file_path = "../data/operations.xlsx"
     filtered_transactions = filter_by_time_range(read_xlsx(file_path), input_date)
 
+    # Getting cards numbers used in filtered operations
     cards_info = get_unique_cards(filtered_transactions)
 
+    # Adding the second key and its value to the json_answer
     new_key = "cards"
     nested_dict = get_list_of_cards_info(cards_info, filtered_transactions)
     json_answer[new_key] = nested_dict
 
+    # Adding the third key and its value to the json_answer
     new_key = "top_transactions"
     nested_dict = get_top_transactions(filtered_transactions)
     json_answer[new_key] = nested_dict
 
-    settings = load_user_settings('../user_settings.json')
-    currencies = settings.get('user_currencies', [])
-    stocks = settings.get('user_stocks', [])
+    # Getting data from API requests
+    settings = load_user_settings("../user_settings.json")
+    currencies = settings.get("user_currencies", [])
+    stocks = settings.get("user_stocks", [])
 
+    # Adding the fourth key and its value to the json_answer
     new_key = "currency_rates"
     nested_dict = get_currency_rates(currencies)
     json_answer[new_key] = nested_dict
 
+    # Adding the fifth key and its value to the json_answer
     new_key = "stock_prices"
     nested_dict = get_stock_prices(stocks)
     json_answer[new_key] = nested_dict
 
-    return json_answer
+    # Python object transforms into string in JSON
+    json_str = json.dumps(json_answer, ensure_ascii=False, indent=4)
+
+    return json_str
 
 
 if __name__ == "__main__":
